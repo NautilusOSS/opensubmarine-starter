79b9c91ce36d09bbd5fbf985f96da04997b42efc
bwhdx <benedictdixon@hotmail.co.uk>
2025-01-20 17:01:18 -0700

    Update to starting test file

--- src/scripts/test/contract.test.js
+++ src/scripts/test/contract.test.js
@@ -1,9 +1,7 @@
 import { expect } from "chai";
-import { deploy, algodClient, acc } from "../command.js";
-import { HelloWorldClient } from "../clients/HelloWorldClient.js";
+import { deploy } from "../command.js";
 
 describe("Hello World Testing", function () {
-  this.timeout(60000);
 
   let deployOptions = {
     type: "HelloWorld",
@@ -13,23 +11,14 @@ describe("Hello World Testing", function () {
   let contract;
   let appId;
 
-  // before(async function () {// If deploying a new contract for each test. (DEVNET)
-  //   const { appId, appClient } = await deploy(deployOptions);
-  //   console.log(appId);
-  //   expect(appId).to.not.equal(0);
-  //   contract = appClient;
-  // });
-
-  before(async function () {// If using an already deployed contract. (TESTNET)
-    appId = 12570;
-    contract = await new HelloWorldClient({
-      resolveBy: "id",
-      id: appId,
-      sender: acc,
-    }, algodClient);
+  beforeEach(async function () {
+    const { id, appClient } = await deploy(deployOptions);
+    appId = id;
+    contract = appClient;
+    expect(appId).to.not.equal(0);
   });
 
-  after(async function () {
+  afterEach(async function () {
   });
 
   it("Should return Hello, World!", async function () {
