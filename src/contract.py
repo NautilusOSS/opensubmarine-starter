3a4cc18a207603f43426f2fb612ae0d4eede3b5a
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2025-01-21 11:35:54 -0700

    add os ownable example

--- src/contract.py
+++ src/contract.py
@@ -1,13 +1,30 @@
 from algopy import (
-    ARC4Contract,
-    arc4,
+    Global,
     String,
+    arc4,
 )
 
-class HelloWorld(ARC4Contract):
+from opensubmarine import Ownable
+
+
+class HelloWorld(Ownable):
+    """
+    A simple Hello World smart contract that inherits from Ownable.
+    """
+
     def __init__(self) -> None:
-        pass
+        # ownable state
+        # Ownable has owner state which we must initialize
+        self.owner = Global.creator_address  # set owner to creator
+
 
     @arc4.abimethod
     def hello_world(self) -> String:
         return String("Hello, World!")
+
+    # Ownable implements transfer method to transfer ownership
+    # We can override it to add additional logic
+    # For example, we can make it so that ownership is non-transferable
+    @arc4.abimethod
+    def transfer(self, new_owner: arc4.Address) -> None:
+        pass
